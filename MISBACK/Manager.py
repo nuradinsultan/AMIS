1fe0787d27e36adef4cf100ad592886542609811
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2024-11-14 14:21:11 +0300

    Update Manager.py

--- MISBACK/Manager.py
+++ MISBACK/Manager.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AMIS.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
