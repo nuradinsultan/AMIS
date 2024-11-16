8447319802940e4f90946654c34d9b1138702c70
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2024-11-16 16:17:57 +0300

    Update middleware.py

--- MISBACK/Core/middleware.py
+++ MISBACK/Core/middleware.py
@@ -1,1 +1,8 @@
+# backend/core/middleware.py
 
+from django.utils.deprecation import MiddlewareMixin
+
+class CustomHeaderMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        response['X-Custom-Header'] = 'My Custom Header'
+        return response
