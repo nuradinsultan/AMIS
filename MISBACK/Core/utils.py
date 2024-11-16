ef9cbc8966280ee5507b80e90f0dcbad1cf8d9a2
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2024-11-16 16:36:07 +0300

    Update utils.py

--- MISBACK/Core/utils.py
+++ MISBACK/Core/utils.py
@@ -1,1 +1,22 @@
+# backend/core/utils.py
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+from django.core.mail import send_mail
+from django.conf import settings
+def get_env_variable(var_name):
+    """
+    Get the environment variable or return an exception.
+    """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = f"Set the {var_name} environment variable"
+        raise ImproperlyConfigured(error_msg)
+
+
+def send_welcome_email(user):
+    subject = "Welcome to Our Medical Information System"
+    message = f"Hi {user.first_name},\n\nThank you for registering. We're glad to have you!"
+    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
 
