98c5c75a779fc16a8e7077a56fda69868e83c631
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2024-11-16 15:36:11 +0300

    Update apps.py

--- MISBACK/inventory/apps.py
+++ MISBACK/inventory/apps.py
@@ -1,1 +1,7 @@
+# backend/inventory/apps.py
 
+from django.apps import AppConfig
+
+class InventoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'inventory'
