4f9416dd5be683d5907a1d2a67426e332429eb2f
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2024-11-16 15:35:26 +0300

    Update admin.py

--- MISBACK/inventory/admin.py
+++ MISBACK/inventory/admin.py
@@ -1,1 +1,9 @@
+# backend/inventory/admin.py
 
+from django.contrib import admin
+from .models import InventoryItem
+
+@admin.register(InventoryItem)
+class InventoryItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'last_updated')
+    search_fields = ('name',)
