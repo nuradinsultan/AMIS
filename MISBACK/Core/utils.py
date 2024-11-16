5daf8475b4a61a1f1a4e686457281c2e800dbc9a
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2024-11-16 16:46:47 +0300

    Update utils.py

--- MISBACK/Core/utils.py
+++ MISBACK/Core/utils.py
@@ -1,9 +1,10 @@
 # backend/core/utils.py
 import os
 from django.core.exceptions import ImproperlyConfigured
-
 from django.core.mail import send_mail
 from django.conf import settings
+from datetime import datetime
+
 def get_env_variable(var_name):
     """
     Get the environment variable or return an exception.
@@ -20,3 +21,11 @@ def send_welcome_email(user):
     message = f"Hi {user.first_name},\n\nThank you for registering. We're glad to have you!"
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
 
+
+def format_date(date, format='%Y-%m-%d'):
+    """
+    Format a datetime object into a string.
+    """
+    if not isinstance(date, datetime):
+        raise ValueError("The provided value must be a datetime object")
+    return date.strftime(format)
