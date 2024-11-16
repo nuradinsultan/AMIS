ee77c758719bce9e84a9c846e7f0705d84de9f4f
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2024-11-16 15:40:15 +0300

    Update views.py

--- MISBACK/inventory/views.py
+++ MISBACK/inventory/views.py
@@ -1,1 +1,32 @@
+# backend/inventory/views.py
 
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import InventoryItem
+from .forms import InventoryItemForm
+
+class InventoryListView(ListView):
+    model = InventoryItem
+    template_name = 'inventory/inventory_list.html'
+    context_object_name = 'inventory_items'
+
+class InventoryDetailView(DetailView):
+    model = InventoryItem
+    template_name = 'inventory/inventory_detail.html'
+
+class InventoryCreateView(CreateView):
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = 'inventory/inventory_form.html'
+    success_url = reverse_lazy('inventory_list')
+
+class InventoryUpdateView(UpdateView):
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = 'inventory/inventory_form.html'
+    success_url = reverse_lazy('inventory_list')
+
+class InventoryDeleteView(DeleteView):
+    model = InventoryItem
+    template_name = 'inventory/inventory_confirm_delete.html'
+    success_url = reverse_lazy('inventory_list')
