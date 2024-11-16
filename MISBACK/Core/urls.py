3c5eae5fc72eedc622d39c7bdf190debf0d6d5de
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2024-11-16 16:27:28 +0300

    Update urls.py

--- MISBACK/Core/urls.py
+++ MISBACK/Core/urls.py
@@ -1,1 +1,10 @@
+# backend/core/urls.py
 
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('inventory/', include('inventory.urls')),
+    # other includes
+]
