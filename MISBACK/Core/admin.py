92faab7b7386d4602e9f4bbfc114132269fd4382
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2024-11-16 16:16:31 +0300

    Update admin.py

--- MISBACK/Core/admin.py
+++ MISBACK/Core/admin.py
@@ -1,1 +1,9 @@
+# backend/core/admin.py
 
+from django.contrib import admin
+from .models import CoreModel
+
+@admin.register(CoreModel)
+class CoreModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
